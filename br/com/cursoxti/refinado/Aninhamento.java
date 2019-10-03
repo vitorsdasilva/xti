93004032a65e9b75e05bce3e480772b92e589a75
Vitor Santos <vitors.dasilva@gmail.com>
2019-10-03 14:46:53 -0300

    Trabalhando com Classes Aninhadas

---
+++ br/com/cursoxti/refinado/Aninhamento.java
@@ -0,0 +1,50 @@
+package br.com.cursoxti.refinado;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Aninhamento extends JFrame {
+	JButton botao;
+	
+	public Aninhamento(){
+
+		super("Aninhamento");
+		
+		botao = new JButton("Ok");
+		botao.addActionListener(new ListenerAninhado());
+		
+		botao.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Classe anonima executa" + botao.getText());
+				
+			}
+		});
+		
+		getContentPane().add(botao);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,300);
+		setVisible(true);
+	}
+	public class ListenerAninhado implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Classe aninhada executa" + botao.getText());
+		}
+		
+	}
+	public static void main(String[] args) {
+		Aninhamento janela = new Aninhamento();
+		
+		Aninhamento.ListenerAninhado listener = 
+				janela.new ListenerAninhado();
+				
+		
+
+	}
+
+}
